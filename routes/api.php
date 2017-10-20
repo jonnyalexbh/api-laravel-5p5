49302e4b48b8c3190ffc4105563e9cb38bc2b1ab
jonnyalexbh <jonnyalex.bh@gmail.com>
2017-10-20 11:22:32 -0500

    simple example with api key

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,12 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
+});
+
+/**
+* countries
+*/
+Route::group(['middleware' => 'auth:api'], function () {
+  Route::resource('countries', 'CountrieController', ['only' => ['index', 'show']]);
 });
